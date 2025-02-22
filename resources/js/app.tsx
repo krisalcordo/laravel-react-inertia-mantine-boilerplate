8816f524af2ddff20c91af5f7501fc0cd9d1314e
Kris Alcordo <kris@alcordo.com>
2025-02-21 23:02:56 -0500

    init

---
+++ resources/js/app.tsx
@@ -0,0 +1,27 @@
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<MantineProvider>
+            <App {...props} />
+        </MantineProvider>);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
